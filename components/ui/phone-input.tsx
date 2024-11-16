08c56cef34af8c10b3cb890d64f55b7a28ae06d4
Aashish Panthi <aashishpanthi11@gmail.com>
2024-11-16 23:26:03 +0545

    fix: type 'string' is not assignable to type 'E164Number' #28 (#35)
    Co-Authored-By: omeralpi <omeralpi@icloud.com>

--- components/ui/phone-input.tsx
+++ components/ui/phone-input.tsx
@@ -53,7 +53,7 @@ const PhoneInput: React.ForwardRefExoticComponent<PhoneInputProps> =
            *
            * @param {E164Number | undefined} value - The entered value
            */
-          onChange={(value) => onChange?.(value || "")}
+          onChange={(value) => onChange?.(value || ("" as RPNInput.Value))}
           {...props}
         />
       );
